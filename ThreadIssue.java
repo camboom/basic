6a72822886d75ef726ca41362c40da3f33c09cb9
camboom <cameronbolourchi@gmail.com>
2014-10-23 16:12:54 -0700

    ThreadIssue.java
    Generate x amount of user processes to test ulimit process maximum.

---
+++ ThreadIssue.java
@@ -0,0 +1,21 @@
+import java.io.*; 
+import java.lang.Thread;
+
+public class ThreadIssue {
+   public static void main(String args[]) {
+      int x=0;
+      while(x<=10){
+         new Thread(new Runnable(){
+             public void run() {
+                 try {
+                     Thread.sleep(10000000);
+                 } catch(InterruptedException e) { }        
+             }    
+         }).start();
+ System.out.println("Fake Thread to test USO issue " + x );
+      x++;
+      }
+
+   }   
+
+}
